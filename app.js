065b8c175e0764c87870c504898016d6589298ce
naalonso29 <naalonso29@gmail.com>
2021-12-07 19:47:33 -0300

    Trabajando con formularios - Completo

--- app.js
+++ app.js
@@ -8,4 +8,8 @@ app.listen(app.get("port"), () => console.log("Servidor Corriendo"));
 
 app.get('/', (req, res) => { res.sendFile(path.join(__dirname, './views', 'home.html')) });
 
+app.get('/register', (req, res) => { res.sendFile(path.join(__dirname, './views', 'register.html')) });
+
+app.get('/login', (req, res) => { res.sendFile(path.join(__dirname, './views', 'login.html')) });
+
 app.use(express.static('./public'));
