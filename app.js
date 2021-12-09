916b5c1beb7b3805c1347e36b4c536451d512711
naalonso29 <naalonso29@gmail.com>
2021-12-09 15:15:20 -0300

    Heroku

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.set("port", 3030);
+app.set("port", 3000);
 
-app.set('port', process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000, () => "Servidor Corriendo")
 
 app.get('/', (req, res) => { res.sendFile(path.join(__dirname, './views', 'home.html')) });
 
